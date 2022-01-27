d3d6de545f47a840e4e78b431a0890c4124e9a31
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-01-27 09:47:20 +0200

    FEAT: Form and css scaling to the login page

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,7 +1,15 @@
+import '../styles/LoginPage.css';
 export default function HomePage() {
-  return <>
-    <img src="./assets/Logo-Hello.png" alt="Hello" />
-    <h1>Lost in translation</h1>
-    <h2>Get started</h2>
-  </>;
+  return <div className="main">
+    <div className="horizontal">
+      <img src="./assets/Logo-Hello.png" alt="Hello" />
+      <div>
+        <h1>Lost in translation</h1>
+        <h2>Get started</h2>
+      </div>
+    </div>
+    <form action="">
+      <input type="text" />
+    </form>
+  </div>;
 }
