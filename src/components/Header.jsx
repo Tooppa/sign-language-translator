354fcb1994d18b0c71c7b6061321b67521221215
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-01-27 12:29:06 +0200

    FEAT: Basic header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import '../styles/Header.css';
 
 export default function Header() {
     return <nav>
--- src/views/LoginPage.jsx
+++ src/views/LoginPage.jsx
@@ -3,7 +3,7 @@ export default function HomePage() {
   return <div className="main">
     <div className="horizontal">
       <img src="./assets/Logo-Hello.png" alt="Hello" />
-      <div>
+      <div className='vertical'>
         <h1>Lost in translation</h1>
         <h2>Get started</h2>
       </div>
