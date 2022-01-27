d57ab32a8715a2b5dc27996ab1c96186e67f669f
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-01-27 09:26:49 +0200

    CHORE: assets can now be accessed from public folder

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,5 +1,7 @@
 export default function HomePage() {
   return <>
-    <h2>Home</h2>
+    <img src="./assets/Logo-Hello.png" alt="Hello" />
+    <h1>Lost in translation</h1>
+    <h2>Get started</h2>
   </>;
 }
