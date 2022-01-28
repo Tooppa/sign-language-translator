ae4df840804e242f7403ff8516e0c577da1f2203
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-01-28 11:41:35 +0200

    FEAT: Signs are now shown in the translation page

--- src/views/LoginPage.jsx
+++ src/views/LoginPage.jsx
@@ -2,7 +2,7 @@ import '../styles/LoginPage.css';
 export default function HomePage() {
   return <div className="color-bg-yellow">
     <div className="horizontal">
-      <img src="./assets/Logo-Hello.png" alt="Hello" />
+      <img className='logo' src="./assets/Logo-Hello.png" alt="Hello" />
       <div className='vertical'>
         <h1>Lost in translation</h1>
         <h2>Get started</h2>
--- src/views/TranslationPage.jsx
+++ src/views/TranslationPage.jsx
@@ -1,19 +1,41 @@
 import '../styles/TranslationPage.css';
+import React, { useState } from 'react';
+import { translateToSignLanguage } from '../services/translationEngine.js'
+
 export default function TranslationPage() {
+  const [text, setText] = useState('');
+  const [translations, setTranslations] = useState([]);
+
+  const Signs = () => {
+    return translations.map((t, index) => {
+      return <div key={t+index}>
+        <img className="signs" src={t} alt="" />
+      </div>
+    });
+  }
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    setTranslations(await translateToSignLanguage(text));
+  }
   return <>
     <div className="color-bg-yellow">
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div className='translateField'>
-          <input type="text" placeholder='Type here to tranlate' />
+          <input
+            type="text"
+            placeholder='Type here to translate'
+            value={text}
+            onChange={e => setText(e.target.value)}
+          />
           <button className='color-bg-purple' type='submit'>-{'>'}</button>
         </div>
       </form>
     </div>
     <div className='textArea'>
       <div className='formBg'>
-        <p className='text'>
-
-        </p>
+        <div className='text'>
+          <Signs />
+        </div>
         <div className='line' />
       </div>
     </div>
