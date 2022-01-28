ecff8c463b4b991a5706bb8eb73eba60abff116b
Mikko <mikko.ryynanen2@gmail.com>
2022-01-28 19:20:31 +0200

    FEAT: Added logging in via Redux to LoginPage

--- src/views/LoginPage.jsx
+++ src/views/LoginPage.jsx
@@ -1,21 +1,43 @@
-import '../styles/LoginPage.css';
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { loginAttemptAction } from "../store/actions/loginActions";
+import "../styles/LoginPage.css";
+
 export default function HomePage() {
-  return <div className="color-bg-yellow">
-    <div className="horizontal">
-      <img className='logo' src="./assets/Logo-Hello.png" alt="Hello" />
-      <div className='vertical'>
-        <h1>Lost in translation</h1>
-        <h2>Get started</h2>
-      </div>
-    </div>
-    <form action="">
-      <div className='formBg'>
-        <div className='inputField'>
-          <input type="text" placeholder='What is your name?' />
-          <button className='color-bg-purple' type='submit'>-{'>'}</button>
+  const session = useSelector((state) => state.session);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const nameSubmitted = (ev) => {
+    ev.preventDefault();
+
+    if (!session.isLoggedIn) {
+      dispatch(loginAttemptAction(ev.target.username.value));
+      navigate("/translation");
+    }
+  };
+
+  return (
+    <div className="color-bg-yellow">
+      <div className="horizontal">
+        <img className="logo" src="./assets/Logo-Hello.png" alt="Hello" />
+        <div className="vertical">
+          <h1>Lost in translation</h1>
+          <h2>Get started</h2>
         </div>
-        <div className='line' />
       </div>
-    </form>
-  </div>;
+      <form onSubmit={nameSubmitted}>
+        <div className="formBg">
+          <div className="inputField">
+            <input type="text" placeholder="What is your name?" name="username"/>
+            <button className="color-bg-purple" type="submit">
+              -{">"}
+            </button>
+          </div>
+          <div className="line" />
+        </div>
+      </form>
+    </div>
+  );
 }
