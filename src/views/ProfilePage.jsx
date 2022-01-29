197da1f1bce9208c3ffade4d51e569ddcb160af0
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-01-29 15:11:06 +0200

    FEAT: Profile now shows translations. also added styling to the list and buttons

--- src/views/ProfilePage.jsx
+++ src/views/ProfilePage.jsx
@@ -1,14 +1,37 @@
+import { useSelector } from 'react-redux';
 import Container from '../components/Container';
 import '../styles/ProfilePage.css';
 
 export default function ProfilePage() {
+  const session = useSelector((state) => state.session);
+
+  const Translations = () => {
+    if (session.user.translations) {
+      return session.user.translations.map((t, index) => {
+        return <li key={t + index} className='listItem'>
+          {t}
+        </li>
+      });
+    }
+    else{
+      return <li/>
+    }
+  }
+
+  const logout = () => {
+
+  }
+  const clearTranslations = () => {
+
+  }
+
   return <>
     <div className="color-bg-yellow">
       <div className="horizontal">
         <img className='robot' src="./assets/Logo.png" alt="Hello" />
         <div className='vertical'>
           <h1>Username: user</h1>
-          <button className='logout'>
+          <button className='logout' onClick={logout}>
             <h2>Logout</h2>
           </button>
         </div>
@@ -17,33 +40,12 @@ export default function ProfilePage() {
     <Container>
       <div className='info'>
         <h1 >Translations</h1>
-        <button className='clear'>
+        <button className='clear' onClick={clearTranslations}>
           <h2>Clear Translations</h2>
         </button>
       </div>
       <ul className='translationList'>
-        <li>test</li>
-        <li>test</li>
-        <li>test</li>
-        <li>test</li>
-        <li>test</li>
-        <li>test</li>
-        <li>test</li>
-        <li>test</li>
-        <li>test</li>
-        <li>test</li>
-        <li>test</li>
-        <li>test</li>
-        <li>test</li>
-        <li>test</li>
-        <li>test</li>
-        <li>test</li>
-        <li>test</li>
-        <li>test</li>
-        <li>test</li>
-        <li>test</li>
-        <li>test</li>
-        <li>test</li>
+        <Translations/>
       </ul>
     </Container>
   </>;
